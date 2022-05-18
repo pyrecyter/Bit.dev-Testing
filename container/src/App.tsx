ecd404fb93a58cd701929a9e16c5e2e4ed233507
sachira_nuwanga <sachiran@zone24x7.com>
2022-05-19 01:29:21 +0530

    added notification component

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import Counter from "@pyrecyter/cportaltest.counter";
+import { showNotification } from "@pyrecyter/cportaltest.notification";
 
 function App() {
   return (
     <div className="App">
       <h1>Loading from bit.dev</h1>
       <Counter initialValue={10} />
+      <h2>Show notification</h2>
+      <button onClick={() => showNotification({ message: "showing test msg" })}>
+        Show Notification
+      </button>
     </div>
   );
 }
--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Counter from "./components/counter";
+import { showNotification } from "./components/notification";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
       <Counter initialValue={11} />
+      <button onClick={() => showNotification({ message: "test message" })}>
+        Show Notification
+      </button>
     </div>
   );
 }
